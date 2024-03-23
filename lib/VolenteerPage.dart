6361fbe29e87e9dc73b8ca0b47a27e2e2dd71f40
AchouakG <Ak.Guennoun@gmail.com>
2024-03-23 21:00:07 +0100

    modified these four files

--- lib/VolenteerPage.dart
+++ lib/VolenteerPage.dart
@@ -2,9 +2,6 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
 class MyApp extends StatelessWidget {
   @override
@@ -20,11 +17,9 @@ class MyApp extends StatelessWidget {
 class My2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
       body: Container(
-        height: screenSize.height,
         decoration: const BoxDecoration(
           color: Color.fromARGB(255, 240, 235, 255),
         ),
@@ -58,6 +53,7 @@ class My2Page extends StatelessWidget {
                 ),
               ),
             ),
+//////////////////////////
             Container(
               // autogrouperap4iPu5 (W2Qstn5G5RcFCX6QxJdZqV)
               padding: EdgeInsets.fromLTRB(16, 19, 12, 0),
@@ -68,7 +64,7 @@ class My2Page extends StatelessWidget {
                   Container(
                     // group59Q1Z (2163:181)
                     margin: EdgeInsets.fromLTRB(0, 0, 3, 18),
-                    padding: EdgeInsets.fromLTRB(0, 10.5, 30.5, 8),
+                    padding: EdgeInsets.fromLTRB(0, 10.5, 0, 10),
                     height: 218,
                     decoration: BoxDecoration(
                       color: Color(0x9e867bc7),
@@ -77,37 +73,64 @@ class My2Page extends StatelessWidget {
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
+                        //////////////////////////
                         Container(
-                          margin: EdgeInsets.fromLTRB(150, 10, 55, 150),
-                          child: Text(
-                            'AUI',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontFamily: 'PollerOne',
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                              height: 1.1875,
-                              color: Color(0xff000000),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          // auivu1 (2147:68)
-                          child: Container(
-                            margin: EdgeInsets.fromLTRB(8, 0, 8, 0),
-                            child: Text(
-                              '# VOLENTEERS',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontFamily: 'Quantico',
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400,
-                                height: 1.43,
-                                color: Color(0xff000000),
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment
+                                .start, // Aligns children to the start (left)
+                            children: [
+                              Container(
+                                margin: EdgeInsets.fromLTRB(150, 10, 0, 0), // Adjust bottom margin for 'AUI'
+                                child: Text(
+                                  'AUI',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: 'PollerOne',
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                    height: 1.1875,
+                                    color: Color(0xff000000),
+                                  ),
+                                ),
                               ),
-                            ),
+                              Row(
+                                children: [
+                                  Container(
+                                    margin:
+                                        EdgeInsets.fromLTRB(15, 90, 118, 12),
+                                    // Adjust right margin for spacing between '# VOLUNTEERS' and '# BLIND'
+                                    child: Text(
+                                      '# VOLUNTEERS',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontFamily: 'Quantico',
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w400,
+                                        height: 1.43,
+                                        color: Color(0xff000000),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(12, 90, 0, 12),
+                                    child: Text(
+                                      '# BLIND',//textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontFamily: 'Quantico',
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w400,
+                                        height: 1.43,
+                                        color: Color(0xff000000),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
+
                         Container(
                           margin: EdgeInsets.fromLTRB(0, 0, 13, 0),
                           height: double.infinity,
@@ -123,42 +146,111 @@ class My2Page extends StatelessWidget {
                 ],
               ),
             ),
-            Container( // yello container
-              
+            Container(
+              // yello container
+
               padding: EdgeInsets.fromLTRB(16, 0, 12, 0),
               width: double.infinity,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Container(
-  margin: EdgeInsets.fromLTRB(0, 0, 3, 18),
-  padding: EdgeInsets.fromLTRB(0, 10.5, 30.5, 9),
-  height: 140,
-  decoration: BoxDecoration(
-    color: Color.fromARGB(158, 219, 225, 134),
-    borderRadius: BorderRadius.circular(20),
-  ),
-  child: Row(
-    crossAxisAlignment: CrossAxisAlignment.end,
-    children: [
-      Padding(
-        padding: EdgeInsets.fromLTRB(8, 0, 30.5, 24), // Adjust padding as needed
-        child: CircleAvatar(
-          backgroundColor: Color.fromARGB(71, 219, 225, 134),
-          backgroundImage: AssetImage('Assets/images/image6.png'),
-          radius: 45, // Adjust the radius here
-        ),
-      ),
-    ],
-  ),
-),
-
+                    margin: EdgeInsets.fromLTRB(0, 0, 3, 18),
+                    padding: EdgeInsets.fromLTRB(0, 10.5, 30.5, 9),
+                    height: 140,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(158, 219, 225, 134),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(
+                              8, 0, 30.5, 24), // Adjust padding as needed
+                          child: CircleAvatar(
+                            backgroundColor: Color.fromARGB(71, 219, 225, 134),
+                            backgroundImage:
+                                AssetImage('Assets/images/image6.png'),
+                            radius: 45, // Adjust the radius here
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemSelected;
+
+  CustomBottomNavigationBar({
+    required this.selectedIndex,
+    required this.onItemSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.blind),
+          label: 'Help',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school),
+          label: 'School',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.groups),
+          label: 'Community',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          label: 'Account',
+        ),
+      ],
+      currentIndex: selectedIndex,
+      selectedItemColor: Color.fromARGB(255, 159, 116, 198),
+      onTap: onItemSelected,
+      type: BottomNavigationBarType.shifting,
+      unselectedItemColor: Color.fromARGB(255, 15, 15, 15),
+      elevation: 10.0,
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final VoidCallback onTap;
+
+  const MenuItem({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: onTap,
     );
   }
 }

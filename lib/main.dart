a1c8e3541caf6dde792c2aa49f7f1fa2b6609df4
AbdessamadAe <ab.aitelmouden@gmail.com>
2024-03-26 12:56:26 +0100

    Added a dummy user to avoid passing Null parameter

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var user;
+    var user = User(
+      firstName: 'John',
+      lastName: 'Doe',
+      phoneNumber: '1234567890',
+      image: 'path/to/image.jpg',
+      email: 'j.Doe@aui.ma'
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Your App Title',

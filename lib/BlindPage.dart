4fc1c614f7966313cce1389dfdce76dd7707f737
AchouakG <Ak.Guennoun@gmail.com>
2024-05-07 12:37:18 +0200

    modifying the blindPage interface

--- lib/BlindPage.dart
+++ lib/BlindPage.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(OnMyWay());
@@ -31,90 +30,60 @@ class _BlindPageState extends State<BlindPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
+        backgroundColor: Color.fromARGB(255, 196, 194, 210),
         body: Center(
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              _isElevated = !_isElevated;
-            });
-          },
-          child:
-          Padding(
-  padding: EdgeInsets.fromLTRB(55, 200, 50, 0),
-  child: Container(
-    height: 320,
-    width: 320,
-    decoration: BoxDecoration(
-      color: Colors.grey[300],
-      shape: BoxShape.circle,
-      boxShadow: _isElevated
-          ? [
-              BoxShadow(
-                color: Colors.black,
-                offset: Offset(4, 4),
-                blurRadius: 15,
-                spreadRadius: 1,
-              ),
-              BoxShadow(
-                color: Colors.white,
-                offset: Offset(-4, -4),
-                blurRadius: 15,
-                spreadRadius: 1,
-              ),
-            ]
-          : null,
-    ),
-    child: Padding(
-      padding: EdgeInsets.all(20), // Adjust padding as needed for the text
-      child: Center(
-        child: Text(
-          'Send Notification', // Button text
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.w700,
-            color: const Color.fromARGB(255, 81, 81, 81),
-          ), // Text style
-        ),
-      ),
-    ),
-  ),
-))
-        
-        //   body:Padding(
-        //   padding: EdgeInsets.fromLTRB(70, 350, 50, 0), // Adjust padding as needed for the circular button
-        //   child: Container(
-        //     width: 250, // Set your desired button width
-        //     height: 250, // Set your desired button height
-        //     decoration: BoxDecoration(
-        //       shape: BoxShape.circle, // Set container shape to circle
-        //       color: Color.fromARGB(255, 201, 223, 240), // Set button background color
-        //       boxShadow: [
-        //         BoxShadow(
-        //           color: Colors.black.withOpacity(0.4), // Add shadow for depth effect
-        //           spreadRadius: 3,
-        //           blurRadius: 5,
-        //           offset: Offset(0, 3),
-        //         ),
-        //       ],
-        //     ),
-        //     child: Padding(
-        //       padding: EdgeInsets.all(20), // Adjust padding as needed for the text
-        //       child: Center(
-        //         child: Text(
-        //           'Press Me', // Button text
-        //           style: TextStyle(
-        //             fontSize: 30,
-        //             fontWeight: FontWeight.w600,
-
-        //             color: const Color.fromARGB(255, 81, 81, 81)),
-        //             // Text style
-        //         ),
-        //       ),
-        //     ),
-        //   ),
-        // )
-
-        ));
+            child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isElevated = !_isElevated;
+                  });
+                },
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(55, 90, 50, 0),
+                  child: Container(
+                    height: 320,
+                    width: 320,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      shape: BoxShape.circle,
+                      boxShadow: _isElevated
+                          ? [
+                              BoxShadow(
+                                color: Colors.black,
+                                offset: Offset(4, 4),
+                                blurRadius: 15,
+                                spreadRadius: 1,
+                              ),
+                              BoxShadow(
+                                color:Color.fromARGB(255, 196, 194, 210),
+                                offset: Offset(-4, -4),
+                                blurRadius: 15,
+                                spreadRadius: 1,
+                              ),
+                            ]
+                          : null,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(
+                          20), // Adjust padding as needed for the text
+                      child: Center(
+                        child: Text(
+                          !_isElevated
+                              ? 'Sent!'
+                              : 'Send Notification', // Button text
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w700,
+                            color: const Color.fromARGB(255, 81, 81, 81),
+                          ), // Text style
+                        ),
+                      ),
+                    ),
+                  ),
+                ))
+            )
+            
+            
+);
   }
 }
